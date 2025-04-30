a7448c827f4ec4e2ca6acc07b43c67a246b7d621
Caner <caner22@posteo.de>
2025-04-30 12:24:10 +0200

    feat(domain): apply fpdart Either for error handling in TodoRepository

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -1,10 +1,11 @@
+import 'package:fpdart/fpdart.dart';
 import 'package:simple_todo_app/domain/entities/todo.dart';
 
 abstract class TodoRepository {
-  Future<List<Todo>> getAllTodos();
-  Future<void> addTodo(Todo todo);
-  Future<void> deleteTodo(String id);
-  Future<void> deleteAllTodos();
-  Future<void> deleteCompletedTodos();
-  Future<void> toggleCompleteTodo(String id);
+  Future<Either<Exception, List<Todo>>> getAllTodos();
+  Future<Either<Exception, void>> addTodo(Todo todo);
+  Future<Either<Exception, void>> deleteTodo(String id);
+  Future<Either<Exception, void>> deleteAllTodos();
+  Future<Either<Exception, void>> deleteCompletedTodos();
+  Future<Either<Exception, void>> toggleCompleteTodo(String id);
 }

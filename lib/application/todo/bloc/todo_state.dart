8f1324976ac62411f90d63c821eb5f16e4b5afcc
Caner <caner22@posteo.de>
2025-05-03 16:52:43 +0200

    refactor: add const constructors to Todo entity and TodoBloc events/states

--- lib/application/todo/bloc/todo_state.dart
+++ lib/application/todo/bloc/todo_state.dart
@@ -3,24 +3,30 @@ part of 'todo_bloc.dart';
 //TODO: Consider to use Equatable
 /// Represents the state of the Todo list.
 @immutable
-sealed class TodoState {}
+sealed class TodoState {
+  const TodoState();
+}
 
 /// Base state class for all Todo states.
-final class TodoInitial extends TodoState {}
+final class TodoInitial extends TodoState {
+  const TodoInitial();
+}
 
 /// State indicating that Todos are being loaded.
-final class TodoLoading extends TodoState {}
+final class TodoLoading extends TodoState {
+  const TodoLoading();
+}
 
 /// State when Todos are successfully loaded.
 final class TodoLoaded extends TodoState {
   final List<Todo> todos;
 
-  TodoLoaded(this.todos);
+  const TodoLoaded(this.todos);
 }
 
 /// State for error handling
 final class TodoError extends TodoState {
   final String message;
 
-  TodoError(this.message);
+  const TodoError(this.message);
 }
